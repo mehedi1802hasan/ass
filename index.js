cd4423cfe1de672de52a7134f85e9e06f6b79368
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-02-18 22:39:12 +0600

    changed-all-done-js

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ document.getElementById('first-btn').addEventListener('click',function(){
   
     const titleField=document.getElementById('first-name').innerText;
     const bField=document.getElementById('first-b-field').value;
-    const hField=document.getElementById('first-h-field').value;
+    const lField=document.getElementById('first-h-field').value;
   if(
-    bField==='' || hField===''|| isNaN(bField) || isNaN(hField)|| bField <0 || hField<0 
+    bField==='' || lField===''|| isNaN(bField) || isNaN(lField)|| bField <0 || lField<0 
   ){
    return alert('Please give me valid number')
   }
 else{
   serial ++;
-  const area= 0.5 *bField * hField  ;
+  const area= 0.5 *bField * lField  ;
   const areaField=area.toFixed(2);
 getTable(titleField,areaField);
 }
@@ -59,27 +59,27 @@ document.getElementById('third-btn').addEventListener('click',function(){
 document.getElementById('fourth-btn').addEventListener('click',function(){
   serial ++;
     const titleField=document.getElementById('fourth-name').innerText;
-    const bField=document.getElementById('fourth-d1-field').innerText;
-    const hField=document.getElementById('fourth-d2-field').innerText;
-    const areaField= 0.5* bField * hField;
+    const d1Field=document.getElementById('fourth-d1-field').innerText;
+    const d2Field=document.getElementById('fourth-d2-field').innerText;
+    const areaField= 0.5* d1Field * d2Field;
   getTable(titleField,areaField);
 
 })
 document.getElementById('five-btn').addEventListener('click',function(){
   serial ++;
     const titleField=document.getElementById('five-name').innerText;
-    const bField=document.getElementById('five-p-field').innerText;
-    const hField=document.getElementById('five-b-field').innerText;
-    const areaField=bField * hField;
+    const pField=document.getElementById('five-p-field').innerText;
+    const bField=document.getElementById('five-b-field').innerText;
+    const areaField=pField * bField;
   getTable(titleField,areaField);
 
 })
 document.getElementById('six-btn').addEventListener('click',function(){
   serial ++;
     const titleField=document.getElementById('six-name').innerText;
-    const bField=document.getElementById('six-a-field').innerText;
-    const hField=document.getElementById('six-b-field').innerText;
-    const area=bField * hField;
+    const aField=document.getElementById('six-a-field').innerText;
+    const bField=document.getElementById('six-b-field').innerText;
+    const area=aField * bField;
     const areaField=area.toFixed(2);
   getTable(titleField,areaField);
 
